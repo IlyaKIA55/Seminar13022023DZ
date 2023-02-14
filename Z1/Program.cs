b85366860fbe765581fd2d54004c03f4994afc5b
Ilya <jyg3a125@gmail.com>
2023-02-14 16:19:45 +0500

    add Z1 and Z2

---
+++ Z1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через запятую");
+string[] strArray = Console.ReadLine().Split(',');
+
+int[] result = new int[strArray.Length];
+int count = 0;
+
+for (int i = 0; i < result.Length; i++)
+{
+    result[i] = int.Parse(strArray[i]);
+    if (result[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество положительных элементов {count}");
